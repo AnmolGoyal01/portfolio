bad8bb572aa33ed2e70b85717953dc9f940dfa8b
anmolgoyal1212 <anmolgoyal_ee21b6_59@dtu.ac.in>
2024-08-05 16:26:31 +0530

    smooth scrolling

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 
 const NavLink = ({ href, title }) => {
+  const handelClick = (e)=>{
+    e.preventDefault();
+    const element = document.querySelector(href);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
   return (
     <Link
       href={href}
+      onClick={e=>handelClick(e)}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
       {title}
