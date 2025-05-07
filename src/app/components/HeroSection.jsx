5e833f592064a0388bbb049d55eafe8d2ade9456
Anmol Goyal <139466193+AnmolGoyal01@users.noreply.github.com>
2025-05-07 13:27:52 +0000

    update resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -55,7 +55,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1CsotIgevsxC2zUn9l167jDW4K3H5EZEO/view?usp=sharing"
+              href="https://drive.google.com/file/d/11kl9zwbSVoPJXxGfSJpQPfvd3KOCO4Zd/view?usp=sharing"
               target="-"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
